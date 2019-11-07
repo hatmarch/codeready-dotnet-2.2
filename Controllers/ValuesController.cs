32d1cdce0d358947bb7130f14af10017eba18fbf
mhildenb <mhildenb@redhat.com>
2019-11-07 01:15:35 +0000

    Change Stuff

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,23 +1,43 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Net.Http;
 
 namespace dotnet.Controllers
 {
-    [Route("/")]
-    public class ValuesController : Controller
-    {
-        const string url = "????";
-        const string responseStringFormat = "customer => {0}\n";
-        // GET api/values
-        [HttpGet]
-        public string Get()
-        {
-            string hostname = Dns.GetHostName();
-            return String.Format(responseStringFormat, hostname);
-        }
-    }
+	[Route("/")]
+	public class ValuesController : Controller
+	{
+		// Bryon is awesome
+		const string url = "http://preference.coles-demo.svc.cluster.local:8080";
+		const string responseStringFormat = "customer {0} => {1}\n";
+
+		static HttpClient client = new HttpClient();
+
+		private string callPreference()
+		{
+			string response = "unknown";
+			try
+			{
+				using (var task = client.GetStringAsync(url))
+				{
+					response = task.Result;
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Error calling preference service: {0}", e.Message);
+			}
+
+			return response;
+		}
+
+		// GET api/values
+		[HttpGet]
+		public string Get()
+		{
+			string hostname = Dns.GetHostName();
+			return String.Format(responseStringFormat, hostname, callPreference());
+		}
+	}
 }
